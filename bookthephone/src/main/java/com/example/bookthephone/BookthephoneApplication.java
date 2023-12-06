cacc392039806d3fc5abb8a3e2694b4c034f5cf8
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-12-06 17:39:39 -0300

    Primeiro commit do projeto
    Neste primeiro commit dei inicio a criação aa entidade de Biblioteca

---
+++ bookthephone/src/main/java/com/example/bookthephone/BookthephoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookthephone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookthephoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookthephoneApplication.class, args);
+	}
+
+}
---
+++ bookthephone/src/main/java/com/example/bookthephone/model/Biblioteca.java
@@ -0,0 +1,46 @@
+package com.example.bookthephone.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Biblioteca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(mappedBy = "biblioteca",cascade = CascadeType.ALL)
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "biblioteca", cascade = CascadeType.ALL)
+    private List<Ebook> ebooks;
+
+    public Biblioteca(){
+        this.ebooks = new ArrayList<>();
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public List<Ebook> getEbooks() {
+        return ebooks;
+    }
+
+    public void setEbooks(List<Ebook> ebooks) {
+        this.ebooks = ebooks;
+    }
+
+}
