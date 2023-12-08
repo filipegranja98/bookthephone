5dc5eaf8a2c8b1bc71f3eaac02de1e8f3e0a2582
AntonyMarcosmak <145510984+AntonyMarcosmak@users.noreply.github.com>
2023-12-07 22:03:05 -0300

    Commit model - carrinho de compras

---
+++ bookthephone/src/main/java/com/example/bookthephone/model/CarrinhoDeCompras.java
@@ -0,0 +1,62 @@
+package com.example.bookthephone.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class CarrinhoDeCompras {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Ebook> ebooks;
+
+    private double valorTotal;
+    private LocalDateTime dataCompra;
+
+    public CarrinhoDeCompras() {
+        
+    }
+
+    public CarrinhoDeCompras(List<Ebook> ebooks, double valorTotal, LocalDateTime dataCompra) {
+        this.ebooks = ebooks;
+        this.valorTotal = valorTotal;
+        this.dataCompra = dataCompra;
+    }
+
+
+   public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ebook> getEbooks() {
+        return ebooks;
+    }
+
+    public void setEbooks(List<Ebook> ebooks) {
+        this.ebooks = ebooks;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDateTime getDataCompra() {
+        return dataCompra;
+    }
+
+    public void setDataCompra(LocalDateTime dataCompra) {
+        this.dataCompra = dataCompra;
+    }
+}
